937de9dda8c661aa2cc6c0ad026737fc4d76e1ba
Perakz <tomislav.perakovic@gmail.com>
2026-05-31 13:38:58 +0200

    Update main.c

--- drone-trajectory-sensing/main.c
+++ drone-trajectory-sensing/main.c
@@ -1,101 +1,57 @@
-/*******************************************************************************
-* File Name:   main.c
-*
-* Description: This is the source code for the Empty Application Example
-*              for ModusToolbox.
-*
-* Related Document: See README.md
-*
-*
-*******************************************************************************
- * (c) 2021-2026, Infineon Technologies AG, or an affiliate of Infineon
- * Technologies AG. All rights reserved.
- * This software, associated documentation and materials ("Software") is
- * owned by Infineon Technologies AG or one of its affiliates ("Infineon")
- * and is protected by and subject to worldwide patent protection, worldwide
- * copyright laws, and international treaty provisions. Therefore, you may use
- * this Software only as provided in the license agreement accompanying the
- * software package from which you obtained this Software. If no license
- * agreement applies, then any use, reproduction, modification, translation, or
- * compilation of this Software is prohibited without the express written
- * permission of Infineon.
- *
- * Disclaimer: UNLESS OTHERWISE EXPRESSLY AGREED WITH INFINEON, THIS SOFTWARE
- * IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING, BUT NOT LIMITED TO, ALL WARRANTIES OF NON-INFRINGEMENT OF
- * THIRD-PARTY RIGHTS AND IMPLIED WARRANTIES SUCH AS WARRANTIES OF FITNESS FOR A
- * SPECIFIC USE/PURPOSE OR MERCHANTABILITY.
- * Infineon reserves the right to make changes to the Software without notice.
- * You are responsible for properly designing, programming, and testing the
- * functionality and safety of your intended application of the Software, as
- * well as complying with any legal requirements related to its use. Infineon
- * does not guarantee that the Software will be free from intrusion, data theft
- * or loss, or other breaches ("Security Breaches"), and Infineon shall have
- * no liability arising out of any Security Breaches. Unless otherwise
- * explicitly approved by Infineon, the Software may not be used in any
- * application where a failure of the Product or any consequences of the use
- * thereof can reasonably be expected to result in personal injury.
-*******************************************************************************/
-
-/*******************************************************************************
-* Header Files
-*******************************************************************************/
-#if defined (CY_USING_HAL)
+#include "cy_pdl.h"
 #include "cyhal.h"
-#endif
 #include "cybsp.h"
+#include "cy_retarget_io.h"
+#include "xensiv_dps3xx_mtb.h"
+#include "xensiv_dps3xx.h"
+#include "math.h"
 
-/******************************************************************************
-* Macros
-*******************************************************************************/
+#define OVERSAMPLING            7
+#define I2C_MASTER_FREQUENCY    400000
 
-
+#define R 287.05f
+#define G 9.80665f
+#define REFERENCE_SAMPELING 20
 /*******************************************************************************
 * Global Variables
-*******************************************************************************/
+********************************************************************************/
+/* Context for dps310 */
+xensiv_dps3xx_t dps310_sensor;
 
+/* Declaration for i2c handler */
+cyhal_i2c_t I2Cm_HW;
+
+/* Define the I2C master configuration structure */
+cyhal_i2c_cfg_t i2c_cfg_master = {
+        CYHAL_I2C_MODE_MASTER,
+        0,                          /* address is not used for master mode */
+        I2C_MASTER_FREQUENCY
+};
 
 /*******************************************************************************
-* Function Prototypes
-*******************************************************************************/
-
-
-/*******************************************************************************
-* Function Definitions
-*******************************************************************************/
-
-/*******************************************************************************
-* Function Name: main
-*********************************************************************************
-* Summary:
-* This is the main function for CPU. It...
-*    1.
-*    2.
-*
-* Parameters:
-*  void
-*
-* Return:
-*  int
-*
-*******************************************************************************/
+ * Function Name: main
+ ********************************************************************************
+ * Summary:
+ * This is the main function for CM4 CPU. It perfroms the following opeartions:
+ *    1. Initializes the BSP
+ *    2. Initializes retarget IO for UART debug printing
+ *    3. Initializes I2C using HAL driver
+ *    4. Initializes the DPS310 pressure sensor
+ *    5. Measures the temperature and the pressure values and prints it on the
+*        serial terminal every 1 second.
+ *
+ * Return:
+ *  int
+ *
+ *******************************************************************************/
 int main(void)
 {
     cy_rslt_t result;
-
-#if defined (CY_DEVICE_SECURE) && defined (CY_USING_HAL)
-    cyhal_wdt_t wdt_obj;
-
-    /* Clear watchdog timer so that it doesn't trigger a reset */
-    result = cyhal_wdt_init(&wdt_obj, cyhal_wdt_get_max_timeout_ms());
-    CY_ASSERT(CY_RSLT_SUCCESS == result);
-    cyhal_wdt_free(&wdt_obj);
-#endif
+    uint32_t revisionID = 0;
+    float pressure, temperature;
 
     /* Initialize the device and board peripherals */
     result = cybsp_init();
-
-    /* Board init failed. Stop program execution */
     if (result != CY_RSLT_SUCCESS)
     {
         CY_ASSERT(0);
@@ -104,8 +60,97 @@ int main(void)
     /* Enable global interrupts */
     __enable_irq();
 
+    /* Initialize retarget-io to use the debug UART port */
+    result = cy_retarget_io_init(CYBSP_DEBUG_UART_TX, CYBSP_DEBUG_UART_RX,
+                                CY_RETARGET_IO_BAUDRATE);
+    if (result != CY_RSLT_SUCCESS)
+    {
+        CY_ASSERT(0);
+    }
+
+    /* \x1b[2J\x1b[;H - ANSI ESC sequence to clear screen. */
+    printf("\x1b[2J\x1b[;H \r");
+    printf("=========================================================\n\r");
+    printf("  PSoC 6 MCU:  Interfacing DPS310 Pressure Sensor \r\n");
+    printf("=========================================================\n\n\r");
+
+    /* Initialize i2c for pressure sensor */
+    result = cyhal_i2c_init(&I2Cm_HW, CYBSP_I2C_SDA, CYBSP_I2C_SCL, NULL);
+    if (result != CY_RSLT_SUCCESS)
+    {
+        printf("\r\nI2C initialization failed\r\n");
+        CY_ASSERT(0);
+    }
+
+    /* Configure i2c with master configurations */
+    result = cyhal_i2c_configure(&I2Cm_HW, &i2c_cfg_master);
+    if (result != CY_RSLT_SUCCESS)
+    {
+        printf("\r\nFailed to configure I2C\r\n");
+        CY_ASSERT(0);
+    }
+
+    /* Initialize pressure sensor */
+    result = xensiv_dps3xx_mtb_init_i2c(&dps310_sensor, &I2Cm_HW,
+                                        XENSIV_DPS3XX_I2C_ADDR_DEFAULT);
+    if (result != CY_RSLT_SUCCESS)
+    {
+        printf("\r\nFailed to initialize DPS310 I2C\r\n");
+        CY_ASSERT(0);
+    }
+
+    /* Retrieve the DPS310 Revision ID and display the same */
+    if (xensiv_dps3xx_get_revision_id(&dps310_sensor,(uint8_t*)&revisionID) == CY_RSLT_SUCCESS)
+    {
+        printf("DPS310 Revision ID = %d\r\n\n",(uint8_t)revisionID);
+    }
+    else
+    {
+        printf("Failed to get Revision ID\r\n");
+        CY_ASSERT(0);
+    }
+
+	float p0 = 0.0f;
+	float t0 = 0.0f;
+
+	float p0_sum = 0.0f;
+	float t0_sum = 0.0f;
+
+	for (int i = 0; i < REFERENCE_SAMPELING; i++) {
+		float p01;
+		float t01;
+		xensiv_dps3xx_read(&dps310_sensor, &p01, &t01);
+		p0_sum = p0_sum + p01;
+		t0_sum = t0_sum + t01;
+	}
+	p0 = p0_sum / REFERENCE_SAMPELING;
+	t0 = t0_sum / REFERENCE_SAMPELING;
+	t0 = t0 + 273.15f;
+	
+
     for (;;)
     {
+        /* Read the pressure and temperature data */
+	
+        if (xensiv_dps3xx_read(&dps310_sensor, &pressure, &temperature) == CY_RSLT_SUCCESS)
+        {
+            /* Display the pressure and temperature data in console*/
+            printf("Pressure : %0.2f mBar", pressure);
+            /* 0xF8 - ASCII Degree Symbol */
+            printf("\t Temperature: %0.2f %cC \r\n", temperature, 0xF8);
+			temperature = temperature + 273.15f;
+			float z = 0.0f;
+			z = (R / G) * ((temperature + t0) / 2) * logf(p0/pressure);
+			printf ("Height : %0.2f m", z);
+        }
+        else
+        {
+            printf("\n Failed to read temperature and pressure data.\r\n");
+            CY_ASSERT(0);
+        }
+        
+        /* Generate a delay of 1 second before next read */
+        cyhal_system_delay_ms(1000);
     }
 }
 
